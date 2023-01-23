affc9ccab7841efa567a0bf755f6b2279b83f132
Amirth24 <amirth300324@gmail.com>
2023-01-23 22:07:39 +0530

    the object oriented approach is replaced with procedural.
    deleted class DigitClassifier entirely added predict method and the
    method reduce is still in progress.

--- app.py
+++ app.py
@@ -1,76 +1,51 @@
-import os
-import cv2
 from PIL import Image as im
 import numpy as np
-from utils import dict_to_ndarray
 from flask import Flask, render_template, request, send_from_directory
 
 from tensorflow.keras import models
+import cv2
 
 app = Flask(__name__)
 
-class DigitClassifier:
-    def __init__(self, image = None) -> None:
-        self.image = image or np.zeros((20, 20, 20)) 
-        self.model = models.load_model('digit_recognizer_2.h5')
-        print('Model Loaded Sucessfully!')
+model = models.load_model('output/digit_recognizer_2.h5')
+
+def reduce_img(img):
+    img_3 = img[:, :, :-1]
+    print(img.shape)
+
+    gray_img = cv2.cvtColor(img_3, cv2.COLOR_RGB2GRAY)
+    gray_img_resized = cv2.resize(gray_img, (28,28))
+    print(gray_img.shape)
+
+def predict(img: im.Image) -> int:
+
     
-    def show(self):
-        cv2.imshow('image', cv2.cvtColor(np.array(self.image), cv2.COLOR_RGB2BGR))
-        cv2.waitKey(0)
-        
-    def update(self, image, width, height):
-        self.image = image
-        self.width = width
-        self.height = height
-    
-    @staticmethod
-    def reduced_image(image, width, height):
-        assert width >= 50 and height >= 50 ,"Cannot reduce more than this"
-        image  = cv2.GaussianBlur(image,(9,15),cv2.BORDER_DEFAULT)
-        scale_percent = 10 # percent of original size
-        width = int(image.shape[1] * scale_percent / 100)
-        height = int(image.shape[0] * scale_percent / 100)
-        dim = (width, height)
-        reduced = cv2.resize(image, dim, interpolation = cv2.INTER_AREA)
-        reduced =cv2.cvtColor(reduced, cv2.COLOR_BGR2GRAY)
-        return reduced, reduced.shape
-
-    def show_reduced(self):
-        reduced, _ = DigitClassifier.reduced_image(np.array(self.image), self.width, self.height)
-        cv2.imshow('reduced', reduced)
-        cv2.waitKey(0)
-
-    def save_img(self):
-        self.image('test.png')
-
-    def save_reduced(self):
-        reduced, _ = DigitClassifier.reduced_image(np.array(self.image), self.width, self.height)
-        im.fromarray(reduced).save('reduced.png')
+    img = img.resize((28,28))
 
 
-    def predict(self):
-        image, reduced_shape  = DigitClassifier.reduced_image(np.array(self.image), self.width, self.height)
+    img = img.convert('L')
 
-        print(image.shape)
-        prediction = self.model.predict(image.reshape(1, reduced_shape[0] , reduced_shape[1]))
-
-        print(np.argmax(prediction))
-
-digit_classifier = DigitClassifier()
+    img = np.asarray(img)
 
 
+    img = img/255.0
+
+    res = model.predict(img.reshape(1, 28, 28, 1))
+
+
+    print(np.argmax(res))
 
 # Receiving Data
 @app.post('/postimage')
 def post_data():
-    global digit_classifier
     request_data = request.get_json()
     width, height = request_data['width'],request_data['height']
-    image = dict_to_ndarray(request_data['data'],width, height , 4)
-    digit_classifier.update(im.fromarray(image.astype(np.uint8)), width, height)
-    digit_classifier.save_reduced()
-    digit_classifier.predict()
+    # image = im.fromarray(np.array([int(x) for _ , x in request_data['data'].items()]).resize(width, height, 4))
+
+    img = np.array([x for _,x in request_data['data'].items()]).reshape((width, height, 4))
+    print(img.shape)
+
+    reduce_img(img)
 
     return "200"
 
@@ -79,9 +54,6 @@ def post_data():
 def index():
     return render_template('index.html')
 
-@app.route('/favicon.ico')
-def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'),'favicon.ico')
 
 
 
--- utils.py
+++
@@ -1,3 +0,0 @@
-import numpy as np
-def dict_to_ndarray(data, width, height, channels = 3):
-    return np.array([int(x) for _, x in data.items()]).reshape(width, height, channels)
